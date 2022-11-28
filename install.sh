63415db23dafc9b55eb9ca6ad40a8c472fc807d7
agogus228 <108329069+agogus228@users.noreply.github.com>
2022-11-29 00:22:23 +0400

    test
    test

---
+++ install.sh
@@ -0,0 +1,11 @@
+
+echo -e "\nMercuryMC - сборка на оптимизацию Minecraft: Java edition."
+read -r -p "Это первая альфа-версия установщика MercuryMC. Продолжить? (y/N)? " answer
+case ${answer:0:1} in
+    y|Y)
+
+                InstallMercury/options/InstallMercuryMC;;
+    *)
+
+
+esac
