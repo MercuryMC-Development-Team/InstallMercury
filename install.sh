900bdae37414e183cf12c984ce5741e44ae085b0
agogus228 <108329069+agogus228@users.noreply.github.com>
2022-11-29 19:35:54 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-
+#!/bin/bash
 echo -e "\nMercuryMC - сборка на оптимизацию Minecraft: Java edition."
 read -r -p "Это первая альфа-версия установщика MercuryMC. Продолжить? (y/N)? " answer
 case ${answer:0:1} in
     y|Y)
 
-                InstallMercury/InstallMercury/optional_mods/InstallMercuryMC.sh;;
+                InstallMercury/InstallMercury/mods/MercuryMC-1.19.2/optional_mods/InstallMercuryMC.sh;;
     *)
 
 
